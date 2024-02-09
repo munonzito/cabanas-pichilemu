b3a59168eb1cccb68849d8c0c893c6c5bcd1d2c8
Martin Muñoz <martin.munoz.a@gmail.com>
2024-02-09 10:33:36 -0300

    first

---
+++ components/ItemCard.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <div class="w-full h-96 flex flex-col items-center relative">
+                <div class="w-80 h-72 rounded-lg shadow-lg relative group">
+                    <img :src="photo" class="w-80 h-72 object-cover rounded-lg" alt=""/>
+                    <div class="absolute inset-0 flex justify-center items-center bg-black bg-opacity-40 opacity-0 group-hover:opacity-100 rounded-lg cursor-pointer">
+                        <span class="text-white font-semibold">Ver más</span>
+                    </div>
+                    <div class="absolute flex bottom-0 right-0 m-5 p-1 bg-white rounded-full">
+                        <img src="https://media.discordapp.net/attachments/1074418812641882162/1205502126898552832/blue_check.png?ex=65d89a58&is=65c62558&hm=1163d4287af6abe94e4be6f0b8e430c72b64ce3295d9a347f1121838cf75d19c&=&format=webp&quality=lossless&width=450&height=450   " class="w-5 h-5" alt=""/>
+                        <span class="text-xs ml-0.5 mt-0.5">Verificada</span>
+                    </div>
+                </div>
+                <div class="w-80 mt-2">
+                <h2 class="text-black font-semibold">{{  title  }}</h2>
+                <p class="text-gray-500">{{  description  }}</p>
+                </div>
+                
+        </div>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    title: String,
+    description: String,
+    photo: String
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="w-full">
+        <div class="h-16 bg-blue-500 flex items-center justify-center">
+            <h1 class="text-white font-semibold text-2xl">Cabañas en Pichilemu</h1>
+        </div>
+       
+        <div class="flex justify-center p-5">
+            <div class="w-full lg:grid lg:grid-cols-4 lg:gap-5 md:grid md:grid-cols-2 md:gap-5 sm:flex sm:items-center sm:justify-center">
+                <ItemCard v-for="item in items" :title="item.title" :photo="item.photo" :description="item.description"/>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+const items = [
+    {
+        "title": "Cabañas Brisas del Mar",
+        "description": "Cabañas equipadas a 5 minutos de la playaaaaaaaaa",
+        "photo":"https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020"
+    },
+    {
+        "title": "Cabañas San Nicolás",
+        "description": "Cabañas equipadas a 5 minutos de la playa",
+        "photo":"https://www.cabanassannicolas.cl/img/caba61.jpg"
+    },
+    {
+        "title": "Cabañas Brisas del Mar",
+        "description": "Cabañas equipadas a 5 minutos de la playa",
+        "photo":"https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020"
+    },
+    {
+        "title": "Cabañas Brisas del Mar",
+        "description": "Cabañas equipadas a 5 minutos de la playa",
+        "photo":"https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020"
+    },
+    {
+        "title": "Cabañas Brisas del Mar",
+        "description": "Cabañas equipadas a 5 minutos de la playa",
+        "photo":"https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020"
+    },
+    {
+        "title": "Cabañas Brisas del Mar",
+        "description": "Cabañas equipadas a 5 minutos de la playa",
+        "photo":"https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020"
+    }   
+]
+</script>
+
+<style scoped>
+
+</style>
