0c7a219cfaa7ebfa7a96520c7ec1f924b0455363
Martin Muñoz <martin.munoz.a@gmail.com>
2024-02-12 17:09:17 -0300

    web

---
+++ pages/cabanas/[cabana_id].vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="w-full flex flex-col">
+        <div class="h-16 bg-white flex items-center px-5 shadow-lg">
+            <Icon name="material-symbols:arrow-back-ios-new" size="20px" class="text-black"/>
+            <h1 class="text-black font-semibold text-2xl pl-5">Cabañas en Pichilemu</h1>
+        </div>
+        <div class="w-full flex flex-col items-center justify-center p-10">
+            <div class="w-full lg:w-[1024px]">
+                <div class="flex items-center justify-center w-full">
+                    <img class="rounded-lg w-[600px] h-[392px] shadow-lg mr-2 object-cover  lg:rounded-none lg:rounded-l-lg" src="https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020" />
+                    <div class="hidden grid-cols-2 gap-2 lg:grid">
+                        <img class="h-48 shadow-lg object-cover" src="https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020" />
+                        <img class="h-48 shadow-lg object-cover rounded-tr-lg" src="https://www.brisasdelmarpichilemu.cl/slide/10.jpg" />
+                        <img class="h-48 shadow-lg object-cover" src="https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020" />
+                        <img class="h-48 shadow-lg object-cover rounded-br-lg" src="https://lh3.googleusercontent.com/p/AF1QipM8VFcDogp1OKZ0gpHw3u_kbPu2AGL7J5ou4zTg=s1360-w1360-h1020" />
+                    </div>
+                </div>
+                <h2 class="text-black font-semibold text-2xl mt-5">Cabañas Brisas del Mar</h2>
+                <div class="flex items center my-2">
+                    <img class="w-6 h-6" src="https://media.discordapp.net/attachments/1074418812641882162/1205502126898552832/blue_check.png?ex=65d89a58&is=65c62558&hm=1163d4287af6abe94e4be6f0b8e430c72b64ce3295d9a347f1121838cf75d19c&=&format=webp&quality=lossless&width=450&height=450" alt=""/>
+                    <span class="text-md ml-0.5">Verificada</span>
+                </div>
+                <p class="text-gray-500">Cabañas equipadas a 5 minutos de la Playa La Caletilla</p>
+                <!-- divider-->
+                <div class="border-b-2 border-gray-200 my-5"></div>
+                <!-- Ubicación y Mapa -->
+                <div class="">
+                    <h2 class="text-black font-semibold text-2xl my-5">Ubicación</h2>
+        <iframe class="w-full h-96 lg:w-[1024px]" style="border:0" Loading="lazy" allowfullscreen
+src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJc5Ud1WFXYZYRO5Ygo4TCXIA&key=AIzaSyB6YZIeL0Esj8PUgNSXx0qBpAg1kVkYTO4"></iframe>
+                </div>  
+            </div>
+            <div>
+            </div>       
+        </div>
+       
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,43 @@
 <template>
     <div class="w-full">
-        <div class="h-16 bg-blue-500 flex items-center justify-center">
-            <h1 class="text-white font-semibold text-2xl">Cabañas en Pichilemu</h1>
+        <div class="h-24 flex flex-col items-center justify-between px-5 md:flex-row md:px-10 ">
+    <!-- Logo e Información a la izquierda -->
+    <div class="hidden md:flex items-center justify-center w-full md:w-max">
+        <h1 class="font-semibold text-3xl">Cabañas en Pichilemu</h1>
+    </div>
+    
+    <!-- Barra de Búsqueda al Centro -->
+    <div class="absolute left-5 top-8">
+            <!-- Sidebar-->
+            <div class="flex md:hidden items-center">
+                <Icon name="material-symbols:menu" size="30px" class="text-black"/>
+            </div>
         </div>
+    <div class="flex items-center justify-center w-full my-5 md:w-1/3">
+        <div class="w-72 flex md:w-full items-center bg-white rounded-full px-5 py-2 shadow-lg border-2">
+            <input type="text" placeholder="Buscar cabañas..." class="flex-grow px-4 rounded-l-full text-gray-700 leading-tight focus:outline-none">
+            <button class="flex justify-center items-center w-10 h-10 min-w-10x bg-red-500 rounded-full p-2 hover:bg-red-600 focus:outline-none">
+                <Icon name="material-symbols:search" size="20px" class="text-white"/>
+            </button>
+        </div>
+</div>
+
+    
+    
+    <!-- Navegación y Acciones a la derecha -->
+    <div class="flex items-center">
+        <nav class="hidden lg:flex lg:items-center lg:space-x-4">
+            <a href="#about" class="hover:text-blue-300">Sobre Nosotros</a>
+            <a href="#contact" class="hover:text-blue-300">Contacto</a>
+            <button class="bg-red-500 text-white px-4 py-2 rounded-full hover:bg-red-600 focus:outline-none">Publicar Cabaña</button>
+        </nav>
+        
+    </div>
+    
+</div>
+<div class="flex md:hidden items-center justify-center w-full md:w-max">
+        <h1 class="font-semibold text-3xl">Cabañas en Pichilemu</h1>
+    </div>
        
         <div class="flex justify-center p-5">
             <div class="w-full lg:grid lg:grid-cols-4 lg:gap-5 md:grid md:grid-cols-2 md:gap-5 sm:flex sm:items-center sm:justify-center">
